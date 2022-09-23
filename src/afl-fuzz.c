fb0fdcb6144b8804ef6583b3562791e7e071dbd1
artifact11 <zhaoyiru@whu.edu.cn>
2022-09-23 10:55:12 +0800

    z

--- src/afl-fuzz.c
+++ src/afl-fuzz.c
@@ -133,7 +133,7 @@ static u32 select_best_child(afl_state_t * afl)
   child = afl->tree_tmp->first_child;
   while(child)
   {
-    child->UCT = (child->q - Q_min)/(Q_max - Q_min) + 1.414* sqrt(log((double)(child->N)) / ((double)(afl->tree_tmp->N)));
+    child->UCT = (child->q - Q_min)/(Q_max - Q_min) + 1.414* sqrt(log((double)(tree_tmp->N)) / ((double)(afl->child->N)));
     if(child->UCT > UCT_max)
     {
       flag = 1;
